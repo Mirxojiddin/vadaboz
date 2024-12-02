33752ce7a68ac88ea0c46b025bbaf4c9fe8829d9
Mirxojiddin Otajonov <umixrojiddin@gmail.com>
2024-12-02 13:18:12 +0500

    requirments

--- conf/settings.py
+++ conf/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 LOGIN_URL = "accounts:login"
 # Application definition
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
