913efc0d523591733dd3eac915acb8ece71023d6
Mirxojiddin Otajonov <umixrojiddin@gmail.com>
2024-12-03 15:44:17 +0500

    Update form.py

--- promise/form.py
+++ promise/form.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.forms import DateInput
 from promise.models import Promise, PromiseCommit
 
 
@@ -8,11 +8,21 @@ class PromiseCreateForm(forms.ModelForm):
 		model = Promise
 		fields = ('title', 'body', 'public', 'finished_at')
 
+	finished_at = forms.DateField(
+        widget=DateInput(attrs={'type': 'date'}),  # HTML5 'date' input type
+        required=False
+    )
+
 
 class PromiseUpdateForm(forms.ModelForm):
 	class Meta:
 		model = Promise
 		fields = ['title', 'body', 'status', 'finished_at', 'public']
+		
+		finished_at = forms.DateField(
+        widget=DateInput(attrs={'type': 'date'}),  # HTML5 'date' input type
+        required=False
+   		)
 
 
 class PromiseCommitForm(forms.ModelForm):
